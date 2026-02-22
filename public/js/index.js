51ac37fff0d8838fcf9e004d1d9f3f7f377c15f0
Marcella Bagniefski <marceybee@gmail.com>
2026-02-22 15:11:48 -0500

    added api

--- public/js/index.js
+++ public/js/index.js
@@ -5,34 +5,52 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
 
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
+async function getRandomImage() {
+    const client_id = "YOUR_ACCESS_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-    {
-        quote: "You shouldn't put ketchup on your hot dog.",
-        author: "Barack Obama",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
     }
-]
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard",
+//     },
+
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//     },
+
+//     {
+//         quote: "You shouldn't put ketchup on your hot dog.",
+//         author: "Barack Obama",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
